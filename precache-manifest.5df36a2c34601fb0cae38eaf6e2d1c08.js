02168f97dfff38c58f5b8bec35a36a56450099b0
sanchocreativo <sanchocreativo@gmail.com>
2022-04-17 14:36:58 -0300

    Updates

---
+++ precache-manifest.5df36a2c34601fb0cae38eaf6e2d1c08.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8affc701ade5ae99868c538bfc2c27",
+    "url": "/reddit-clone/index.html"
+  },
+  {
+    "revision": "70ca56b5e0a0b70baccc",
+    "url": "/reddit-clone/static/css/PostsLoader.a4472f37.chunk.css"
+  },
+  {
+    "revision": "727a7236a8b603cbaa9e",
+    "url": "/reddit-clone/static/css/home.5db7a605.chunk.css"
+  },
+  {
+    "revision": "404a36dfc21191353fd7",
+    "url": "/reddit-clone/static/css/main.10c86981.chunk.css"
+  },
+  {
+    "revision": "e21bc36d18c69762661d",
+    "url": "/reddit-clone/static/css/posts.9c2a8735.chunk.css"
+  },
+  {
+    "revision": "49f1e2e5ca346fa29741",
+    "url": "/reddit-clone/static/js/5.03c07fe5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reddit-clone/static/js/5.03c07fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8c6a810f9e409263c8",
+    "url": "/reddit-clone/static/js/6.5488580e.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/reddit-clone/static/js/6.5488580e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20dba240a6cf9d13e58f",
+    "url": "/reddit-clone/static/js/7.cad2cd28.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/reddit-clone/static/js/7.cad2cd28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ca56b5e0a0b70baccc",
+    "url": "/reddit-clone/static/js/PostsLoader.a70f316a.chunk.js"
+  },
+  {
+    "revision": "727a7236a8b603cbaa9e",
+    "url": "/reddit-clone/static/js/home.4a5942db.chunk.js"
+  },
+  {
+    "revision": "404a36dfc21191353fd7",
+    "url": "/reddit-clone/static/js/main.8b78a615.chunk.js"
+  },
+  {
+    "revision": "e21bc36d18c69762661d",
+    "url": "/reddit-clone/static/js/posts.30761116.chunk.js"
+  },
+  {
+    "revision": "20470332758146f1896d",
+    "url": "/reddit-clone/static/js/runtime-main.b9f5c2f2.js"
+  }
+]);
